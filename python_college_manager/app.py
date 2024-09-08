3d081ab84df0c44089a042e6b5bce5f1c515d3fc
Santiago Chavarro <santichav417@gmail.com>
2024-09-08 00:23:32 -0500

    first commit and some changes in readme and app

--- python_college_manager/app.py
+++ python_college_manager/app.py
@@ -1,6 +1,20 @@
-from dotenv import load_dotenv
 import streamlit as st
+import pandas as pd
 
-load_dotenv()
+def _extract_students_from_excel(excel_file):
+        """Extracts student information from the provided Excel file."""
+        try:
+            df = pd.read_excel(excel_file)
+        except Exception as e:
+            st.write(f"Error reading the Excel file: {e}")
+            return []
+        st.write(df)
 
-st.title("College grades app")
+
+st.title("Upload students attendance list Excel file")
+
+uploaded_file = st.file_uploader("Attendance list Excel file", type=["xls", "xlsx"])
+
+if uploaded_file is not None:
+    st.write("file was uploaded successfully.")
+    _extract_students_from_excel(uploaded_file)
